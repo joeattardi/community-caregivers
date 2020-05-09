ad0a6710ade5d271576890e50877c9940f0f291c
Joe Attardi <jattardi@gmail.com>
2020-05-09 13:44:49 -0400

    Hide Facebook preview on mobile

--- src/components/FacebookPageEmbed.js
+++ src/components/FacebookPageEmbed.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function FacebookPageEmbed() {
   return (
     <iframe
+      title="Community Caregivers US Facebook page"
       src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcommunitycaregiversus&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=611690179424452"
       width="340"
       height="300"
